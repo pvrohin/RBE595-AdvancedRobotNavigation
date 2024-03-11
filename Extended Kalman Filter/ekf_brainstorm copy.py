dfd57c5e6e37e5e01c98438171447e6a4fffeef8
pvrohin <pvrohin@gmail.com>
2024-03-11 16:36:07 -0400

    debugging the process model

--- Extended Kalman Filter/ekf_brainstorm copy.py
+++ Extended Kalman Filter/ekf_brainstorm copy.py
@@ -4,7 +4,62 @@ import scipy.io
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 
-# Function to load data from .mat file
+# dt = sp.symbols('dt')
+
+# p1, p2, p3, q1, q2, q3, p_dot1, p_dot2, p_dot3, bg1, bg2, bg3, ba1, ba2, ba3 = sp.symbols('p1 p2 p3 q1 q2 q3 p_dot1 p_dot2 p_dot3 bg1 bg2 bg3 ba1 ba2 ba3')
+
+# # Define symbolic variables
+# #phi, theta, psi = sp.symbols('phi theta psi')
+
+# # Define the matrix elements
+# G_q = sp.Matrix([
+#     [sp.cos(q2), 0, -sp.cos(q1)*sp.sin(q2)],
+#     [0, 1, sp.sin(q1)],
+#     [sp.sin(q2), 0, sp.cos(q1)*sp.cos(q2)]
+# ])
+
+# # Compute the inverse of G_q
+# G_q_inv = G_q.inv()
+
+# # Write R_q as a 3x3 matrix just like G_q
+# R_q = sp.Matrix([
+#                 [ sp.cos(q3)*sp.cos(q2) - sp.sin(q1)*sp.sin(q2)*sp.sin(q3), -sp.cos(q1)*sp.sin(q3), sp.cos(q3)*sp.sin(q2) + sp.cos(q2)*sp.sin(q1)*sp.sin(q3)],
+#                 [ sp.cos(q3)*sp.sin(q1)*sp.sin(q2) + sp.cos(q2)*sp.sin(q3), sp.cos(q1)*sp.cos(q3), sp.sin(q3)*sp.sin(q2) - sp.cos(q3)*sp.cos(q2)*sp.sin(q1)],
+#                 [ -sp.cos(q1)*sp.sin(q2), sp.sin(q1), sp.cos(q1)*sp.cos(q2)]
+#                 ])
+
+# # Define the state vector x = [p, q, p_dot, bg, ba]
+# x = sp.Matrix([p1, p2, p3, q1, q2, q3, p_dot1, p_dot2, p_dot3, bg1, bg2, bg3, ba1, ba2, ba3])
+
+# # Create a new matrix including only p_dot1, p_dot2, p_dot3
+# p_dot = sp.Matrix([p_dot1, p_dot2, p_dot3])
+
+# # Define the input vector u = [wx, wy, wz, vx, vy, vz]
+# wx, wy, wz, vx, vy, vz = sp.symbols('wx wy wz vx vy vz')
+# u = sp.Matrix([wx, wy, wz, vx, vy, vz])
+
+# uw = sp.Matrix([wx, wy, wz])
+# ua = sp.Matrix([vx, vy, vz])
+
+# # Define the gravity vector
+# g = sp.Matrix([0, 0, -9.81])
+
+# nbg = sp.Matrix([0, 0, 0])
+# nba = sp.Matrix([0, 0, 0])
+
+# # Define the x_dot equation x_dot = f(x, u) = [p_dot, G_q_inv * u, g + R_q * u, 0, 0]
+# x_dot = sp.Matrix([p_dot, G_q_inv * uw, g + R_q * ua, nbg, nba])
+
+# F = x + x_dot*dt
+
+# # Compute the Jacobian of the process model
+# Jacobian_J = F.jacobian(x)
+
+# # Substitute F with sample values
+# F = F.subs({p1: 0.0, p2: 0, p3: 0, q1: 0, q2: 0, q3: 0, p_dot1: 0, p_dot2: 0, p_dot3: 0, bg1: 0, bg2: 0, bg3: 0, ba1: 0, ba2: 0, ba3: 0, wx: 0, wy: 0, wz: 0, vx: 0, vy: 0, vz: 0, dt: 0.01})
+
+# print(F)
+
 def load_data(filename):
     return scipy.io.loadmat(filename, simplify_cells=True)
 
@@ -57,6 +112,7 @@ def calculate_symbolic():
     Jacobian_J = F.jacobian(x)
 
     return F, Jacobian_J    
+
 
 def process_model(x, delta_t, u):
     # Process model function
@@ -121,17 +177,8 @@ for i in range(len(data['data'])):
 
 Q = np.eye(15) * 0.001  # Process noise covariance matrix (adjust as needed)
 
-data['vicon'] = np.array(data['vicon'])
-#  Transpose it
-data['vicon'] = data['vicon'].T
-
 # Initial state estimate and covariance
-# Intitialize x to the first ground truth position
 x = np.zeros(15)  # Initialize state vector (adjust as needed)
-x[:3] = data['vicon'][0][:3]
-x[3:6] = data['vicon'][0][3:6]
-x[6:9] = data['vicon'][0][6:9]
-x[9:] = 0
 P = np.eye(15) * 0.01  # Initialize covariance matrix (adjust as needed)
 
 estimated_positions = []
@@ -158,8 +205,13 @@ for i in range(len(data['data'])-1):
     # Store estimated and ground truth positions
     estimated_positions.append(x[:3])  # Extract position from state estimate
 
+
 # Convert lists to numpy arrays
 estimated_positions = np.array(estimated_positions)
+
+data['vicon'] = np.array(data['vicon'])
+#  Transpose it
+data['vicon'] = data['vicon'].T
 
 ground_truth_positions = []
 ground_truth_orientations = []
